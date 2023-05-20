6aa3156ccbe91c4b9bec50ced2bf28978c07d2a0
ihala99 <133688355+ihala99@users.noreply.github.com>
2023-05-21 00:32:59 +0300

    Add files via upload

---
+++ additional challenge hala.py
@@ -0,0 +1,21 @@
+contactTable=[{"name":"Ahmed","Phone":"0551112222"},
+              {"name":"Saad","Phone":"0551113333"},
+              {"name":"Sultan","Phone":"0551114444"},
+              {"name":"Morad","Phone":"0551115555"},
+              {"name":"Abdullah","Phone":"0551116666"}] 
+
+
+val=input("Enter phone number:\n")
+if not val.isdigit() or len(val) !=10:
+    print("This is invalid number")
+else:
+
+ for dic in contactTable:
+
+    if dic["phone"]==val:
+       returnname=dic["name"]
+       print(returnname)
+       break
+
+ else:
+    print("Not Found")
