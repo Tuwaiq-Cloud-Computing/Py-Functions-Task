d776b10ad2806c69becfce39b42ef77ee6ec1ef0
shahadsami <142797783+shahadsami@users.noreply.github.com>
2023-09-12 22:08:26 +0300

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,71 @@
+# Lab 3 PYTHON
+
+# ***********Additional Challenge:#***********
+# ******#*******************************************
+# contactTable=[{"name":"Ahmed","Phone":"0551112222"},
+#               {"name":"Saad","Phone":"0551113333"},
+#               {"name":"Sultan","Phone":"0551114444"},
+#               {"name":"Morad","Phone":"0551115555"},
+#               {"name":"Abdullah","Phone":"0551116666"}]
+
+# def phoneNUMBER(num):
+# #  if len(num) !=10 or not num.isdigit():
+# #    return "sorry "
+#   for i in contactTable:
+#    if i["Phone"] == num:
+#     return i["name"]
+#   return "not found"
+
+# # the call :
+# num = input("enter a phone number")
+# name = find_name(num)
+# print(name)
+
+# **************************************
+
+# 1 -----------------------------------
+# def sumab(a, b):
+#     return a + b
+# n = sumab(List[0], List[2])
+# print(n)
+
+# 2 -----------------------------------
+# for i in list:
+#   print(i)
+
+
+# 3-----------------------------------
+# allSum = sum(List)
+# print(allSum)
+
+# 4------------------------------------
+# List.sort()
+# print(List[-1])
+
+# 5---------------------------------------
+# def get_partial_list(List, number):
+#     return List[:number]
+# print(get_partial_list(List,3))
+
+# 6--------------------------------------
+# world = "Tuwaiq_Academy"
+# for i in world:
+#     print(i)
+
+# 7---------------------------------------
+# list = ["Python", "C++", "Java"]
+# x = list[0]
+# for i in list:
+#   if i != "C++":
+#     print(i)
+
+# 8---------------------------------------
+# list=[1,2,0,3,4,0]
+# def arrangeList(list):
+#  zeros = [i for i in list if i == 0]
+#  non_zeros = [i for i in list if i != 0]
+#  return non_zeros + zeros
+
+# #the call:#
+# newList = arrangeList(list)
+# print(newList)
