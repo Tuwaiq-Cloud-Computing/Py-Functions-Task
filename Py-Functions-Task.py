d567368376aa589151577cf2c62ffb7a0af9a098
lujain2000c <lujain.i.k20@gmail.com>
2023-11-22 13:09:48 +0300

    Lujain

---
+++ Py-Functions-Task.py
@@ -0,0 +1,34 @@
+import math
+
+List = [1,2,3,4]
+
+#Question 1
+
+def sum (num1,num2):
+    result = num1 + num2
+    return(result)
+
+#Question 2
+#for i in List:
+ #   print(i)
+
+#Question 3
+#print(sum(List))
+
+#Question 4
+print(max(List))
+
+#Question 5
+def y (list,num):
+ return(list[0:num])
+
+#Question 6
+name = "Tuwaiq_Academy"
+for i in name:
+  print(i)
+
+list2 = ["Python", "C++", "Java"]
+for x in list2:
+   if x == "C++":
+     break
+   print(x)
